705179e400620faa7fb9b990a821357d4fb0ccf9
coldrice-jpg <al9817071@gmail.com>
2025-03-03 16:30:19 -0600

    Add files via upload

---
+++ imprimirvalores.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    std::string nombre = "Juan";
+    int edad = 34;
+
+    std::cout << "El nombre es " << nombre << " de edad " << edad;
+    return 0;
+}
---
+++ ingresarvalores.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    std::string NOMBRE_INGRESADO;
+    std::string EDAD_INGRESADA;
+    std::cin >> NOMBRE_INGRESADO;
+    std::cin >> EDAD_INGRESADA;
+    std::cout << "El nombre es " << NOMBRE_INGRESADO << " de edad " << EDAD_INGRESADA;
+
+    return 0;
+}
